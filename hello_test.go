e2f8fcacffffe85bc9f07cd33950e3f7af6fd468
yashraj-sup <yttfc29062024@gmail.com>
2026-06-29 20:32:10 +0530

    Add name parameter to Hello function

--- hello_test.go
+++ hello_test.go
@@ -1,8 +1,8 @@
 package main
 import "testing"
-func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+func TestHello(t *testing.T){
+	got := Hello("Chris")
+	want := "Hello, Chris"
 	if got!=want{
 		t.Errorf("got %q want %q",got,want)
 	}
