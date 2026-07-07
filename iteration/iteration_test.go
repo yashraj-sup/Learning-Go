aab1847c616d9b783145a46b6a642308467e0668
yashraj-sup <yttfc29062024@gmail.com>
2026-07-08 00:58:22 +0530

    Parameterize Repeat with repeatCount argument,add multiple test case

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -3,16 +3,30 @@ import (
 	"strings"
 	"testing"
 )
-const repeatCount = 5
-func Repeat(character string) string{
+
+func Repeat(character string, repeatCount int) string{
 	var repeated strings.Builder
 	for i :=0; i< repeatCount; i++{
 		repeated.WriteString(character)
 	}
 	return repeated.String()
 }
+func TestRepeatSeventimes(t *testing.T){
+	repeated := Repeat("a",7)
+	expected := "aaaaaaa"
+	if repeated != expected{
+		t.Errorf("expected %q but got %q", expected , repeated)
+	}
+}
+func TestRepeatThreetimes(t *testing.T){
+	repeated := Repeat("a" , 3)
+	expected := "aaa"
+	if repeated != expected{
+		t.Errorf("expected %q but got %q", expected , repeated)
+	}
+}
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a" , 5)
 	expected := "aaaaa"
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected , repeated)
@@ -20,6 +34,6 @@ func TestRepeat(t *testing.T) {
 }
 func BenchmarkRepeat(b *testing.B) {
 	for b.Loop() {
-		Repeat ("a")
+		Repeat ("a" , 5)
 	}
 }
