45e4be98e25f4bb7886e30b9691500be88640935
yashraj-sup <yttfc29062024@gmail.com>
2026-07-08 21:24:13 +0530

    Add SumAll and SumAllTails with checkSums helpers,very heavy section

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,8 +1,28 @@
 package arrays
+func SumAll(numbersToSum...[]int) []int{
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return sums
+}
 func Sum(numbers []int) int{
 	sum := 0
 	for _, number := range numbers  {
 		sum += number
 	}
 	return sum
+}
+func SumAllTails(numbersToSum...[]int) []int{
+	var sums []int
+	for _, numbers :=range numbersToSum {
+		if len(numbers) == 0{
+			sums = append(sums,0)
+		}else{
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
 }
