42d03f2443b867c731d06d668c8874d243fb60ae
yashraj-sup <yttfc29062024@gmail.com>
2026-07-09 17:33:33 +0530

    add table tests to iterate and t.Run to easy debugging

--- structs/shapes.go
+++ structs/shapes.go
@@ -19,3 +19,13 @@ type Circle struct{
 func (c Circle) Area() float64 { //declaring methods
 	return math.Pi * c.Radius * c.Radius
 }
+type Shape interface {
+	Area() float64
+}
+type Triangle struct{
+	Base float64
+	Height float64
+}
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
+}
--- structs/structs_test.go
+++ structs/structs_test.go
@@ -11,20 +11,23 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 func TestArea(t *testing.T) {
-	t.Run("rectangles", func(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := rectangle.Area() //method callinggggg
-	want := 100.00
-		if got != want {
-		t.Errorf("got %g want %g", got, want)
-		}
-	})
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{4.0}
-		got := circle.Area() //method calling
-		want := 50.26548245743669
-			if got != want {
-			t.Errorf("got %g  want %g", got, want)
+	areaTests := []struct{
+		name string
+		shape Shape
+		hasArea float64
+	}{
+		{name:"Rectangle", shape:Rectangle{Width:10.0, Height:10.0}, hasArea:100.00},
+		{name:"Circle", shape:Circle{Radius:4.0}, hasArea: 50.26548245743669},
+		{name:"Triangle", shape:Triangle{Base:4, Height: 6}, hasArea: 12.0},
+	}
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf(" %#v got %g want %g", got, tt.shape, tt.hasArea)
 			}
-	})
+		})
+	}
 }
+
+
